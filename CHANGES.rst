2925b69a5fa93935ddd1d22b32cc4612aeca733f
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-01 00:18:19 +0200

    starting 0.1.2 dev

---
+++ CHANGES.rst
@@ -0,0 +1,10 @@
+v0.1.2 (next version)
+---------------------
+
+- ``DTEND`` property fixes (was one day too early — #3)
+
+
+v0.1.1 (current version)
+------------------------
+
+- first public release
--- edt2ics/__init__.py
+++ edt2ics/__init__.py
@@ -4,7 +4,7 @@
 from edt2ics.ical import iCalSchedule
 from edt2ics.scraper import ScheduleScraper
 
-__version__ = '0.1.1'
+__version__ = '0.1.2-alpha1'
 
 # silent pyflakes
 iCalSchedule, ScheduleScraper
