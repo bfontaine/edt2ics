6670105b86d3b8b98acd2ad4a0ba31dcc7a1e88c
Baptiste Fontaine <batifon@yahoo.fr>
2014-09-30 18:40:08 +0200

    simple test added

---
+++ tests/test_scraper.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from edt2ics.scraper import ScheduleScraper
+
+def noop(*args, **kwargs):
+    pass
+
+class TestScraper(unittest.TestCase):
+
+    def setUp(self):
+        self._fetch_mth = ScheduleScraper._fetch
+        ScheduleScraper._fetch = noop
+
+
+    def tearDown(self):
+        ScheduleScraper._fetch = self._fetch_mth
+
+
+    # __init__
+
+    def test_init_with_year(self):
+        year = 'L3'
+        s = ScheduleScraper(year=year)
+        self.assertEquals(year, s.year)
+
