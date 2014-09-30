f865d5dbb4ee41be087feafbd2b2372968688307
Baptiste Fontaine <batifon@yahoo.fr>
2014-09-30 19:55:33 +0200

    iCalSchedule: scraper arg is optional

--- edt2ics/ical.py
+++ edt2ics/ical.py
@@ -13,7 +13,7 @@ class iCalSchedule(object):
 
     DAYS = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
 
-    def __init__(self, scraper, startdate=None, enddate=None):
+    def __init__(self, scraper=None, startdate=None, enddate=None):
         if startdate is None or enddate is None:
             dts, dte = self._get_dates(scraper.period, scraper.year,
                     scraper.semester)
@@ -27,8 +27,9 @@ class iCalSchedule(object):
         self.enddate = enddate
         self._first_weekdays = {} # cache
         self._init_ical()
-        for ev in scraper.get_events():
-            self.add_event(ev)
+        if scraper is not None:
+            for ev in scraper.get_events():
+                self.add_event(ev)
 
 
     def _init_ical(self):
