ca175e9868485db4da47b25dc59200dc96e67916
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-02 10:43:06 +0200

    some ical tests

--- tests/test_ical.py
+++ tests/test_ical.py
@@ -1,8 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-from datetime import date
-from tempfile import NamedTemporaryFile
-from os import remove
+from datetime import date, time
+from icalendar import Calendar
 
 try:
     import unittest2 as unittest
@@ -11,6 +10,7 @@ except ImportError:
 
 from edt2ics.events import ScheduleEvent
 from edt2ics.ical import iCalSchedule
+
 
 class FakeScraper(object):
 
@@ -28,6 +28,49 @@ class FakeScraper(object):
 
 class TestICal(unittest.TestCase):
 
+
+    def setUp(self):
+        self.ev = {
+            'day': 1,
+            'tstart': time(10, 30),
+            'tend': time(11, 30),
+            'type_': 'cours',
+            'title': 'Foo',
+            'room': '9 3/4',
+            'description': '...'}
+
+    # __init__
+
+    def test_init_without_events(self):
+        ics = iCalSchedule(FakeScraper(events=[]))
+        self.assertEquals(0, len(list(ics.cal.subcomponents)))
+
+    def test_init_with_events(self):
+        ics = iCalSchedule(FakeScraper(events=[self.ev]))
+        self.assertEquals(1, len(list(ics.cal.subcomponents)))
+
+    # _init_ical
+
+    def test_init_ical_set_properties(self):
+        ics = iCalSchedule(FakeScraper())
+        self.assertSequenceEqual(['VERSION', 'PRODID', 'METHOD'],
+                list(ics.cal.keys()))
+
+    # _recur_params
+
+    def test_recur_params_week_start_on_monday(self):
+        ics = iCalSchedule(FakeScraper())
+        self.assertEquals('MO', ics._recur_params(4)['wkst'])
+
+    def test_recur_params_until_enddate_plus_one_day(self):
+        d1 = date(2014, 10, 2)
+        d2 = date(2014, 10, 3)
+        ics = iCalSchedule(FakeScraper(), enddate=d1)
+        params = ics._recur_params(2)
+        self.assertEquals(d2, params['until'])
+
+    # _get_dates
+
     # _str2date
 
     def test_str2date_correct_date(self):
@@ -35,3 +78,25 @@ class TestICal(unittest.TestCase):
         d1 = date(2014, 9, 30)
         d2 = ics._str2date('%d-%d-%d' % (d1.year, d1.month, d1.day))
         self.assertEquals(d1, d2)
+
+    def test_str2date_incorrect_date_raises_exception(self):
+        ics = iCalSchedule(FakeScraper())
+        self.assertRaises(ValueError, lambda: ics._str2date('2014-02-30'))
+
+
+    # add_event
+
+    def test_add_event_set_uid(self):
+        ics = iCalSchedule(FakeScraper())
+        ics.add_event(ScheduleEvent(**self.ev))
+        ev = ics.cal.subcomponents[0]
+        self.assertIn('UID', ev.keys())
+
+
+    # to_ical
+
+    def test_to_ical(self):
+        ics = iCalSchedule(FakeScraper(events=[self.ev]))
+        cal1 = ics.cal
+        cal2 = Calendar.from_ical(ics.to_ical())
+        self.assertEquals(cal1, cal2)
